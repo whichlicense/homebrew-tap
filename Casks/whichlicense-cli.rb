b25fce233c55b0e620a8a890967900af9865167d
David Greven <opensource@grevend.dev>
2023-04-20 22:11:09 +0200

    Bump version to v0.1.3

--- Casks/whichlicense-cli.rb
+++ Casks/whichlicense-cli.rb
@@ -1,10 +1,10 @@
 cask "whichlicense-cli" do
     arch arm: "aarch64", intel: "amd64"
 
-    version "0.0.0"
+    version "0.1.3"
 
-    sha256 arm:   "dda8d2e1d27ce8166bbe27be7af28b7eb9bb35971a79f6e5f8a0d45003df70ab",
-           intel: "f6b107b9677623d2a14810905c145d8a27302a0ffec902adf6b8704d1bc57809"
+    sha256 arm:   "d753ac604542c7f670c46af2c6d09e0fbda8e6791700b8c4ff8cbab4e32b6919",
+           intel: "8aec18dfe678d29db26abb04eaada69e4134c87135d3ae8ebeb2c4231c7fad74"
     
     url "https://github.com/whichlicense/cli/releases/download/v#{version}/whichlicense-darwin-#{arch}"
     name "WhichLicense CLI #{version}"
